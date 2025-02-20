676db328d76f813b248054f254b498ad421ccf67
BuildTools <unconfigured@null.spigotmc.org>
2025-02-20 03:10:36 -0600

    chapter 4 end

--- src/main/java/com/vig/catalogservice/HomeController.java
+++ src/main/java/com/vig/catalogservice/HomeController.java
@@ -1,14 +1,18 @@
 package com.vig.catalogservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @Value("${polar.greeting}")
+    String greeting;
+
     @GetMapping("/")
     public String getGereeting(){
-        return "Welcome to Book Catalog from BooksToBorrow";
+        return "Welcome to Book Catalog from BooksToBorrow" + greeting;
     }
     
 }
